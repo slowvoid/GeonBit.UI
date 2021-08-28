ff674672ef2ea4fae319dab0fbc4e51da17aa57c
johny <johny@DESKTOP-ALUGM9L>
2021-08-28 01:08:56 -0300

    Adicionar arquivos de projeto.

---
+++ GeonBit.UI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GeonBit.UI
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using ( var game = new GeonBitUI_Examples() )
+                game.Run();
+        }
+    }
+}
